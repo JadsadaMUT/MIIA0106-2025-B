90ad19695dad0e252a15d1801d6eef1eba8d3ad4
JadsadaMUT <6811860011@mut.ac.th>
2025-12-01 01:37:43 +0700

    การทดลองครั้งที่2

---
+++ LAB02/TEST01/TEST01.cpp
@@ -0,0 +1,19 @@
+// TEST01.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "Hello LAB2!\n";
+
+    std::cout << "Name: Jadsada Phongrungruengchai\n";
+    std::cout << "Course: MIIA0106\n"
+    std::cout << "Email: 6811860011@mut.ac.th\n";
+    
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Start Debugging menu
